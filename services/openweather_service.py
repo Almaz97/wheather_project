4075a8eff386c0073c719b3dce496c31c1faa272
Almaz97 <almaz.carato@gmail.com>
2022-04-18 22:20:53 +0600

    Integrated with openweathermap.org api

---
+++ services/openweather_service.py
@@ -0,0 +1,17 @@
+from typing import Optional
+import requests
+
+api_key: Optional[str] = None
+
+
+def get_report(city, state, country: Optional[str], units: str) -> dict:
+    if state:
+        q = f"{city},{state},{country}"
+    else:
+        q = f"{city},{country}"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={q}&appid={api_key}&units={units}"
+    resp = requests.get(url)
+    resp.raise_for_status()
+    data = resp.json()
+    forecast = data["main"]
+    return forecast
